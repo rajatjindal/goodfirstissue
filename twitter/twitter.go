4acf32b389cd32975080295acd77c445dca2a5fc
Franco Montini <77291236+devmontini@users.noreply.github.com>
2023-04-14 03:59:58 -0300

    Add unit tests for Tweet method in twitter.Client (#33)

--- twitter/twitter.go
+++ twitter/twitter.go
@@ -1,12 +1,16 @@
 package twitter
 
 import (
+	"errors"
 	"github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"github.com/sirupsen/logrus"
 )
 
-//Tokens is for twitter tokens
+// ErrTweetFailed is for failed tweet
+var ErrTweetFailed = errors.New("failed to send tweet")
+
+// Tokens is for twitter tokens
 type Tokens struct {
 	ConsumerKey   string `yaml:"consumerKey"`
 	ConsumerToken string `yaml:"consumerToken"`
@@ -14,12 +18,12 @@ type Tokens struct {
 	TokenSecret   string `yaml:"tokenSecret"`
 }
 
-//Client is twitter client
+// Client is twitter client
 type Client struct {
 	twitter *twitter.Client
 }
 
-//NewClient returns new twitter client
+// NewClient returns new twitter client
 func NewClient(t *Tokens) (*Client, error) {
 	config := oauth1.NewConfig(t.ConsumerKey, t.ConsumerToken)
 	token := oauth1.NewToken(t.Token, t.TokenSecret)
@@ -31,11 +35,12 @@ func NewClient(t *Tokens) (*Client, error) {
 	}, nil
 }
 
-//Tweet tweets
-func (c *Client) Tweet(msg string) {
+// Tweet tweets
+func (c *Client) Tweet(msg string) error {
 	_, _, err := c.twitter.Statuses.Update(msg, nil)
 	if err != nil {
 		logrus.Error(err)
-		return
+		return err //some error
 	}
+	return nil
 }
